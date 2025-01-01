9ffce390fe3d72d86ebaa974e4f4dd39a75846fa
jayden <jaydenshelton@yahoo.com>
2025-01-01 14:36:09 -0500

    inital commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen border border-red-500 flex justify-center items-center">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
